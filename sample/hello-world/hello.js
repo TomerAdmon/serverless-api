73fc09195b73a403eabf688c3dd7f49b4481105e
Tomer Admon <tomer.a@qualisystems.com>
2023-03-21 17:49:02 -0400

    Update hello.js

--- sample/hello-world/hello.js
+++ sample/hello-world/hello.js
@@ -2,7 +2,7 @@
 
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
-  let responseMessage = 'Hello, Tomer!';
+  let responseMessage = 'Hello, Brian!';
 
   if (event.queryStringParameters && event.queryStringParameters['Name']) {
     responseMessage = 'Terraform Webinar Update: ' + event.queryStringParameters['Name'] + '!';
