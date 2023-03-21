07bb334fd9af085854601f0bc34f818728968fbd
Tomer Admon <tomer.a@qualisystems.com>
2023-03-21 18:26:01 -0400

    Update main.tf

--- sample/main.tf
+++ sample/main.tf
@@ -2,13 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "random_id" "lambda_bucket_name" {
+resource "random_pet" "lambda_bucket_name" {
   length = 4
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = random_pet.lambda_bucket_name.id
-
+  bucket = "serverless-api-${random_pet.lambda_bucket_name.id}"
 }
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
